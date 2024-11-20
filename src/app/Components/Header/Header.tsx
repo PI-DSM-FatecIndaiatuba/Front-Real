2fc789bb385f8e073605038ad46efa79bea3f706
Ramon.god <ramongodinho01@gmail.com>
2024-11-20 13:30:55 -0300

    fix: pequenos ajustes no navbar

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -16,8 +16,8 @@ export const Header = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src= "../../assets/Indaiatuba_brasao.png" alt="" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <img className="h-10 w-10 mr-2" src="../../assets/Indaiatuba_brasao.png" alt="" />
+            <span className="text-xl tracking-tight">Dengue</span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
@@ -28,13 +28,13 @@ export const Header = () => {
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
             <a href="#" className="py-2 px-3 border rounded-md">
-              Sign In
+              Casos de Dengue
             </a>
             <a
               href="#"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
-              Create an account
+              Cadastro de Interesse
             </a>
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
@@ -54,13 +54,13 @@ export const Header = () => {
             </ul>
             <div className="flex space-x-6">
               <a href="#" className="py-2 px-3 border rounded-md">
-                Sign In
+                Casos de Dengue
               </a>
               <a
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
-                Create an account
+                Cadastro de Interesse
               </a>
             </div>
           </div>
